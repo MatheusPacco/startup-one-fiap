8d385d26396f2d63d001b7e67b135177dedc8abc
MatheusPacco <matheuspacco2014@hotmail.com>
2023-09-27 14:16:24 -0300

    FIX: I inseriralidação nos atributos

--- demoSpring/src/main/java/com/example/demospring/model/Receita.java
+++ demoSpring/src/main/java/com/example/demospring/model/Receita.java
@@ -1,6 +1,8 @@
 package com.example.demospring.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
@@ -10,10 +12,15 @@ public class Receita {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Necessário inserir a descrição")
     private String descricao;
+
+    @NotNull
     private LocalDate dataPrescricao;
 
     @ManyToOne
+    @NotNull(message = "Necessário inserir um paciente válido")
     @JoinColumn(name = "id_paciente")
     private Paciente paciente;
 
